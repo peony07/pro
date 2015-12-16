4c2566e25060299df0e4ac7d8c54553b44004452
peony-home <490022533@qq.com>
2015-12-16 14:49:08 +0800

    youhua

--- pro-parent/pro-web/src/main/java/com/sc/pro/web/controller/IndexController.java
+++ pro-parent/pro-web/src/main/java/com/sc/pro/web/controller/IndexController.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.sc.pro.web.common.RestFulClientUtil;
 
-
 /**
  * Created by Administrator on 2015/8/22 0022.
  */
@@ -22,26 +21,26 @@ public class IndexController {
 		_log.info("/index/");
 		// 创建模型跟视图，用于渲染页面。并且指定要返回的页面为index页面
 		ModelAndView mav = new ModelAndView("index");
-		
-		String serverHost="localhost";
-		String serverPort="8009";
-		
-		String baseUrl="http://"+serverHost+":"+serverPort;
-		String url = baseUrl+"/restservice/services/users/getArchUser/";
+
+		String serverHost = "localhost";
+		String serverPort = "8009";
+
+		String baseUrl = "http://" + serverHost + ":" + serverPort;
+		String url = baseUrl + "/restservice/services/users/getArchUser/";
 		String params = "{\"account\":\"admin\"}";
 		String result = RestFulClientUtil.sendPost(url, params);
 		_log.info(result);
 		mav.addObject("result1", result);
 
-		url = baseUrl+"/restservice/services/registry/getName/";
-		 params = "{\"name\":\"测试中文\"}";
-		 result = RestFulClientUtil.sendPost(url, params);
+		url = baseUrl + "/restservice/services/registry/getName/";
+		params = "{\"name\":\"测试中文\"}";
+		result = RestFulClientUtil.sendPost(url, params);
 		_log.info(result);
 		mav.addObject("result2", result);
-		
-		 url = baseUrl+"/restservice/services/open/srv/route/";
-		 params = "{\"service_code\":\"SOHO_APP_PROJECT_LIST\",\"language\":\"zh_CN\"}";
-		 result = RestFulClientUtil.sendPost(url, params);
+
+		url = baseUrl + "/restservice/services/open/srv/route/";
+		params = "{\"service_code\":\"SOHO_APP_PROJECT_LIST\",\"language\":\"zh_CN\"}";
+		result = RestFulClientUtil.sendPost(url, params);
 		_log.info(result);
 
 		mav.addObject("result3", result);
